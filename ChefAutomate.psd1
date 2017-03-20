00b82b1b14a3bd1964e11c45286450790c1e8737
James Bannan <james@bannan.com.au>
2017-03-20 12:44:36 +1100

    Added Get-ChefAutomateComplianceProfiles function

--- ChefAutomate.psd1
+++ ChefAutomate.psd1
@@ -12,7 +12,7 @@
     RootModule = 'ChefAutomate.psm1'
 
     # Version number of this module.
-    ModuleVersion = '0.1.2'
+    ModuleVersion = '0.1.3'
 
     # ID used to uniquely identify this module
     GUID = ''
@@ -66,7 +66,7 @@
     # NestedModules = 
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-    FunctionsToExport = @('Get-ChefAutomateStatus','Set-SessionSecurityProtocol')
+    FunctionsToExport = @('Get-ChefAutomateStatus','Set-SessionSecurityProtocol','Get-ChefAutomateComplianceProfiles')
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport = ''
---
+++ functions/Get-ChefAutomateComplianceProfiles.ps1
@@ -0,0 +1,78 @@
+﻿<#
+
+.SYNOPSIS
+    Returns the compliance profiles for a nominated owner.
+
+.DESCRIPTION
+    Returns the compliance profiles for a nominated owner from the Chef Automate compliance store.
+
+.EXAMPLE
+    C:\PS> Get-ChefAutomateComplianceProfiles -automateServer 'chef-automate.company.com' -automateOrgName OrgName -automateUser admin -automateUserToken <USER-TOKEN>
+
+.INPUTS
+    Takes the DNS-resolvable name of the Chef Automate server (e.g. 'chef-automate' or 'chef-automate.company.com'), the Chef Automate Enterprise name (e.g. OrgName), the Chef Automate user who 'owns' the compliance profiles and the user token for authentication (https://YOUR_AUTOMATE_HOST/e/YOUR_AUTOMATE_ENTERPRISE/#/dashboard?token)
+
+.OUTPUTS
+    PowerShell array of all compliance profiles on the Chef Automate server owned by the nominated user.
+
+#>
+
+function Get-ChefAutomateComplianceProfiles
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        # Chef Automate Server (e.g. chef-automate.company.com)
+        [Parameter(Mandatory=$true,
+                    ValueFromPipelineByPropertyName=$true,
+                    Position=0)]
+        $automateServer,
+
+        # Chef Automate Enterprise Name (e.g. MyOrg)
+        [Parameter(Mandatory=$true,
+                    ValueFromPipelineByPropertyName=$true,
+                    Position=0)]
+        $automateOrgName,
+
+        # Chef Automate User (e.g. 'john')
+        [Parameter(Mandatory=$true,
+                    ValueFromPipelineByPropertyName=$true,
+                    Position=0)]
+        $automateUser,
+
+        # Chef Automate USer Token (https://YOUR_AUTOMATE_HOST/e/YOUR_AUTOMATE_ENTERPRISE/#/dashboard?token)
+        [Parameter(Mandatory=$true,
+                    ValueFromPipelineByPropertyName=$true,
+                    Position=0)]
+        $automateUserToken
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        $complianceURL = 'https://' + $automateServer + '/compliance/profiles/' + $automateUser
+        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $headers.Add("chef-delivery-enterprise", $automateOrgName)
+        $headers.Add("chef-delivery-user", $automateUser)
+        $headers.Add("chef-delivery-token", $automateUserToken)
+        $profiles = Invoke-RestMethod -Method Get -Uri $complianceURL -Headers $headers
+
+        $profileNames = $($profiles | Get-Member -MemberType *Property).Name
+        $names = @()
+        foreach($profileName in $profileNames){
+            $name = New-Object PSObject
+            $name | Add-Member -MemberType NoteProperty -Name 'Name' -Value $profileName
+            $names += $name
+        }
+
+    }
+    End
+    {
+        foreach($name in $names){
+            Write-Output -InputObject $profiles.($name.Name)
+        }
+    }
+}
